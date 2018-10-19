8a275faa12edeccb9994b5c5bd2a3efce2ff8e41
Olafur Gauti Gudmundsson <ogg@bokun.is>
2018-10-19 00:08:44 +0000

    Added swagger definition for new question and checkout services

--- generate.sh
+++ generate.sh
@@ -4,5 +4,5 @@ set -e
 service=$1; shift
 
 set -x #echo on
-/usr/bin/swagger-cli validate bokun.yaml \
-&& /usr/bin/swagger-cli bundle bokun.yaml -o swagger-${service}.json
+swagger-cli validate bokun.yaml \
+&& swagger-cli bundle bokun.yaml -o swagger-${service}.json

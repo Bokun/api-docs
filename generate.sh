328f96b66007b358038d1e964c0dd7014044bde1
Sindri Traustason <sindri@bokun.is>
2019-04-30 10:11:17 +0000

    Added type: object to map objects. Requested by NSP rich connectivity team as this needs to be specified for their code generator.

--- generate.sh
+++ generate.sh
@@ -6,3 +6,7 @@ service=$1; shift
 set -x #echo on
 swagger-cli validate bokun.yaml \
 && swagger-cli bundle bokun.yaml -o swagger-${service}.json
+
+if [[ "$service" = "test" ]]; then
+    sed -i "" 's/api.bokun.io/api.bokuntest.com/' swagger-${service}.json
+fi
